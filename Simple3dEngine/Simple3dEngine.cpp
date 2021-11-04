817bc3b04f32498b8d8ca36eb6159041fe756b69
RainbowHotkey <hotkeysplayer@gmail.com>
2021-11-04 17:46:28 +0300

    Добавьте файлы проекта.

---
+++ Simple3dEngine/Simple3dEngine.cpp
@@ -0,0 +1,26 @@
+﻿#include "Engine/olcConsoleGameEngine.h"
+
+class HotkeysEngine3d : olcConsoleGameEngine
+{
+public:
+    HotkeysEngine3d()
+    {
+
+    }
+
+public:
+    bool OnUserCreate() override
+    {
+
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+
+    }
+};
+
+int main()
+{
+    
+}
