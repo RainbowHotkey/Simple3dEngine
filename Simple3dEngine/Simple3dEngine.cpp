2c71cefd6b10a7fdcb913fcd5cf87193959040b9
RainbowHotkey <hotkeysplayer@gmail.com>
2021-11-04 18:48:04 +0300

    first math formulas. Scaling to view

--- Simple3dEngine/Simple3dEngine.cpp
+++ Simple3dEngine/Simple3dEngine.cpp
@@ -1,26 +1,151 @@
 ﻿#include "Engine/olcConsoleGameEngine.h"
+#include <vector>
 
-class HotkeysEngine3d : olcConsoleGameEngine
+using namespace std;
+
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct triangle
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+struct mat4x4
+{
+    float m[4][4] = { 0 };
+};
+
+
+
+class HotkeysEngine3d : public olcConsoleGameEngine
 {
 public:
     HotkeysEngine3d()
     {
+        m_sAppName = L"3dTest";
+    }
+
+private:
+    mesh meshCube;
+    mat4x4 matProj;
+
+    void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, short c = 0x2588, short col = 0x0000)
+    {
+        DrawLine(x1, y1, x2, y2, c, col);
+        DrawLine(x2, y2, x3, y3, c, col);
+        DrawLine(x3, y3, x1, y1, c, col);
 
     }
+
+    void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
+    {
+        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+        float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+        if (w != 0.0f)
+        {
+            o.x /= w; o.y /= w; o.z /= w;
+        }
+    }
+
 
 public:
     bool OnUserCreate() override
     {
+        meshCube.tris =
+        {
+            // south
+            {0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f},
+            {0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f},
 
+            // east
+            {1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f},
+            {1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f},
+
+            // north
+            {1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f},
+            {1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f},
+
+            // west
+            {0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f},
+            {0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f},
+
+            // top
+            {0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f},
+            {0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f},
+
+            // bottom
+            {1.0f, 0.0f, 0.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f},
+            {1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f},
+
+        };
+
+        // projection matrix
+        float fNear = 0.1f;
+        float fFar = 1000.0f;
+        float fFov = 90.0f;
+        float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+        float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.141519f);
+
+        matProj.m[0][0] = fAspectRatio * fFovRad;
+        matProj.m[1][1] = fFovRad;
+        matProj.m[2][2] = fFar / (fFar - fNear);
+        matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+        matProj.m[2][3] = 1.0f;
+        matProj.m[3][3] = 0.0f;
+
+        return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
+        // draw triangles
+        for (auto tri : meshCube.tris)
+        {
+            triangle triProjected;
+            MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
+            MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
+            MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+
+            // scale into view
+            triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+            triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+            triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+
+            triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+            triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+            triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+
+            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                triProjected.p[1].x, triProjected.p[1].y,
+                triProjected.p[2].x, triProjected.p[2].y,
+                PIXEL_SOLID, FG_WHITE);
+        }
+
+        return true;
     }
 };
 
 int main()
 {
-    
+    HotkeysEngine3d demo;
+    if (demo.ConstructConsole(256, 240, 4, 4))
+        demo.Start();
+
+    return 0;
 }
